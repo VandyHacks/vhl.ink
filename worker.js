b11c2b438cc979eeb2d44eb9b512375f673f3378
Aadi Bajpai <hello@aadibajpai.com>
2021-01-04 18:15:10 +0530

    make code slightly cleaner

--- worker.js
+++ worker.js
@@ -12,11 +12,16 @@ const html = `<!DOCTYPE html>
     <pre>
     use an actual path if you're trying to fetch something.
     send a POST request with form data "url" and "path" if you're trying to put something.
+    set x-preshared-key header for authentication.
     
     source: <a href="https://github.com/VandyHacks/vhl.ink">VandyHacks/vhl.ink</a>
     </pre>
 </body>`
 
+/**
+ * Respond to POST requests with shortened URL creation
+ * @param {Request} request
+ */
 async function handlePOST(request) {
   const psk = request.headers.get("x-preshared-key");
   if (psk !== SECRET_KEY)
@@ -26,17 +31,17 @@ async function handlePOST(request) {
   const data = await request.formData();
   const redirectURL = data.get("url");
   const path = data.get("path");
-  console.log(redirectURL, path);
+
   if (redirectURL === null || path === null) 
     return new Response("`url` and `path` need to be set.", {status: 400});
-  // TODO: figure out failed response format and handling
-  // documentation bleak
+
+  // will overwrite current path if it exists
   await LINKS.put(path, redirectURL);
   return new Response(`${redirectURL} available at ${shortener}${path}`, {status: 201});
 }
 
 /**
- * Respond with hello worker text
+ * Respond to GET requests with redirects
  * @param {Request} request
  */
 async function handleRequest(request) {
@@ -47,9 +52,10 @@ async function handleRequest(request) {
     headers: {
       "content-type": "text/html;charset=UTF-8"
     }})
+ 
   const redirectURL = await LINKS.get(path);
-  if (redirectURL) {
+  if (redirectURL) 
     return Response.redirect(redirectURL, 301);
-  }
+
   return new Response("URL not found. Sad!", {status: 404});
 }
