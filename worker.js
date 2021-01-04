8c4054db7d1d3685d032baf163fc2a71e67eb13f
Matt Leon <ml@mattleon.com>
2021-01-04 12:55:34 -0500

    Stringify resulting list of shortlinks for LIST operation (#3)

--- worker.js
+++ worker.js
@@ -77,7 +77,7 @@ async function handleRequest(request) {
 		const psk = request.headers.get('x-preshared-key');
 		if (psk === SECRET_KEY) {
 			const { keys } = await LINKS.list();
-			return new Response(keys, { status: 200 });
+			return new Response(JSON.stringify(keys), { status: 200 });
 		}
 
 		return new Response(html, {
