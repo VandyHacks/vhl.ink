a866c4626a1053181a95e9e1e9dca4aa058d4d03
Matt Leon <ml@mattleon.com>
2021-01-04 10:10:15 -0500

    Add LIST and DELETE methods

--- worker.js
+++ worker.js
@@ -1,11 +1,17 @@
 addEventListener('fetch', event => {
-  const { request } = event;
+	const { request } = event;
 
-  if (request.method === "POST")
-    return event.respondWith(handlePOST(request))
-
-  return event.respondWith(handleRequest(request))
-})
+	switch (request.method) {
+		case 'POST':
+			return event.respondWith(handlePOST(request));
+		case 'LIST':
+			return event.respondWith(handleLIST(request));
+		case 'DELETE':
+			return event.respondWith(handleDELETE(request));
+		default:
+			return event.respondWith(handleRequest(request));
+	}
+});
 
 const html = `<!DOCTYPE html>
 <body>
@@ -16,28 +22,59 @@ const html = `<!DOCTYPE html>
     
     source: <a href="https://github.com/VandyHacks/vhl.ink">VandyHacks/vhl.ink</a>
     </pre>
-</body>`
+</body>`;
 
 /**
  * Respond to POST requests with shortened URL creation
  * @param {Request} request
  */
 async function handlePOST(request) {
-  const psk = request.headers.get("x-preshared-key");
-  if (psk !== SECRET_KEY)
-    return new Response("Sorry, bad key.", {status: 403})
+	const psk = request.headers.get('x-preshared-key');
+	if (psk !== SECRET_KEY)
+		return new Response('Sorry, bad key.', { status: 403 });
 
-  const shortener = new URL(request.url);
-  const data = await request.formData();
-  const redirectURL = data.get("url");
-  const path = data.get("path");
+	const shortener = new URL(request.url);
+	const data = await request.formData();
+	const redirectURL = data.get('url');
+	const path = data.get('path');
 
-  if (redirectURL === null || path === null) 
-    return new Response("`url` and `path` need to be set.", {status: 400});
+	if (!redirectURL || !path)
+		return new Response('`url` and `path` need to be set.', { status: 400 });
 
-  // will overwrite current path if it exists
-  await LINKS.put(path, redirectURL);
-  return new Response(`${redirectURL} available at ${shortener}${path}`, {status: 201});
+	// will overwrite current path if it exists
+	await LINKS.put(path, redirectURL);
+	return new Response(`${redirectURL} available at ${shortener}${path}`, {
+		status: 201,
+	});
+}
+
+/**
+ * Respond to LIST requests with a list of all registered shortlinks
+ * @param {Request} request
+ */
+async function handleLIST(request) {
+	const psk = request.headers.get('x-preshared-key');
+	if (psk !== SECRET_KEY)
+		return new Response('Sorry, bad key.', { status: 403 });
+
+	const { keys } = await LINKS.list();
+	return new Response(keys, { status: 200 });
+}
+
+/**
+ * Respond to DELETE requests by deleting the shortlink
+ * @param {Request} request
+ */
+async function handleDELETE(request) {
+	const psk = request.headers.get('x-preshared-key');
+	if (psk !== SECRET_KEY)
+		return new Response('Sorry, bad key.', { status: 403 });
+
+	const url = new URL(request.url);
+	const path = url.pathname.split('/')[1];
+	if (!path) return new Response('Not found', { status: 404 });
+	await LINKS.delete(path);
+	return new Response('OK', { status: 200 });
 }
 
 /**
@@ -45,17 +82,17 @@ async function handlePOST(request) {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  const url = new URL(request.url);
-  const path = url.pathname.split("/")[1];
-  if (path === "") 
-    return new Response(html, {
-    headers: {
-      "content-type": "text/html;charset=UTF-8"
-    }})
- 
-  const redirectURL = await LINKS.get(path);
-  if (redirectURL) 
-    return Response.redirect(redirectURL, 301);
+	const url = new URL(request.url);
+	const path = url.pathname.split('/')[1];
+	if (!path)
+		return new Response(html, {
+			headers: {
+				'content-type': 'text/html;charset=UTF-8',
+			},
+		});
 
-  return new Response("URL not found. Sad!", {status: 404});
+	const redirectURL = await LINKS.get(path);
+	if (redirectURL) return Response.redirect(redirectURL, 301);
+
+	return new Response('URL not found. Sad!', { status: 404 });
 }
